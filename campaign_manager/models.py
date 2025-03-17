36f54e090ee48afb70c2bb665406f37084bcc8ab
Wian Spamer <61464034+WSpamer@users.noreply.github.com>
2025-03-17 10:14:04 +0200

    Created base models for campaign_manager

--- campaign_manager/models.py
+++ campaign_manager/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
 # Create your models here.
+class Factions(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=100)
+    description = models.TextField()
+    created = models.DateField()
+    updated = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class OrganizationType(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    type = models.ForeignKey(
+        OrganizationType, on_delete=models.CASCADE)
+    created = models.DateField()
+    updated = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Religion(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    relationship = models.TextField()
+    religion = models.ForeignKey(
+        Religion, on_delete=models.CASCADE)
+    unique_laws = models.TextField()
+    notes = models.TextField()
+    created = models.DateField()
+    updated = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class PointsOfInterest(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    type = models.CharField(max_length=100)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE)
+    description = models.TextField()
+    created = models.DateField()
+    updated = models.DateField()
+
+    def __str__(self):
+        return self.name
