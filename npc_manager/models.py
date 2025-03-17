90d20d1557ae3b7371f1f959a47b2d9c2f236ac9
Wian Spamer <61464034+WSpamer@users.noreply.github.com>
2025-03-17 10:02:04 +0200

    Added basic models for npc_manager

--- npc_manager/models.py
+++ npc_manager/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Occupation(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Race(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class NPC(models.Model):
+    
+    name = models.CharField(max_length=100, unique=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE)
+    occupation = models.ForeignKey(
+        Occupation, on_delete=models.CASCADE)
+    race = models.ForeignKey(
+        Race, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    relationship_level = models.IntegerField()
+    notes = models.TextField()
+    created = models.DateField()
+    updated = models.DateField()
+
+    def __str__(self):
+        return self.name
