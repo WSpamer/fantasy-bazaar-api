e7f6d1590e523199d70f965ce8417fa73a28e48a
Wian Spamer <61464034+WSpamer@users.noreply.github.com>
2025-03-29 13:55:14 +0200

    Added Town & Locations Models

--- campaign_manager/models.py
+++ campaign_manager/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
-class Factions(models.Model):
-    
+class Faction(models.Model):
+
     name = models.CharField(max_length=100, unique=True)
     status = models.CharField(max_length=100)
     description = models.TextField()
@@ -12,8 +13,9 @@ class Factions(models.Model):
     def __str__(self):
         return self.name
 
+
 class OrganizationType(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
 
@@ -22,32 +24,30 @@ class OrganizationType(models.Model):
 
 
 class Organization(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-    type = models.ForeignKey(
-        OrganizationType, on_delete=models.CASCADE)
+    type = models.ForeignKey(OrganizationType, on_delete=models.CASCADE)
     created = models.DateField()
     updated = models.DateField()
 
     def __str__(self):
         return self.name
 
+
 class Religion(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
-    
 
     def __str__(self):
         return self.name
 
 
 class Country(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
     relationship = models.TextField()
-    religion = models.ForeignKey(
-        Religion, on_delete=models.CASCADE)
+    religion = models.ForeignKey(Religion, on_delete=models.CASCADE)
     unique_laws = models.TextField()
     notes = models.TextField()
     created = models.DateField()
@@ -56,15 +56,35 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+
+class Location(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class PointsOfInterest(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
     type = models.CharField(max_length=100)
-    country = models.ForeignKey(
-        Country, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     description = models.TextField()
     created = models.DateField()
     updated = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Town(models.Model):
+
+    name = models.CharField(max_length=100, unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
