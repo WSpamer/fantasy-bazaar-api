cb0d4e2dfc2b6cf8dfc5e012d76cf295161cb8cd
Wian Spamer <61464034+WSpamer@users.noreply.github.com>
2025-03-29 13:55:50 +0200

    Formatting

--- character_manager/models.py
+++ character_manager/models.py
@@ -1,36 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Occupation(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Race(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class NPC(models.Model):
-    
+
     name = models.CharField(max_length=100, unique=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE)
-    occupation = models.ForeignKey(
-        Occupation, on_delete=models.CASCADE)
-    race = models.ForeignKey(
-        Race, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
     gender = models.CharField(max_length=100)
     age = models.IntegerField()
     description = models.TextField()
