479ab267e3eedbbe0db474ceb66ee0e87f56ffff
Wian Spamer <61464034+WSpamer@users.noreply.github.com>
2025-03-29 13:56:02 +0200

    Added models to admin.

--- campaign_manager/admin.py
+++ campaign_manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+admin.site.register(models.Faction)
+admin.site.register(models.OrganizationType)
+admin.site.register(models.Country)
+admin.site.register(models.Religion)
